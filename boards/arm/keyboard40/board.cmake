b557f3936dc4fa3561b41041a347707ff6e04fb1
cinmend <xinmend@outlook.com>
2026-04-25 20:55:13 +0800

    add new device

--- boards/arm/keyboard40/board.cmake
+++ boards/arm/keyboard40/board.cmake
@@ -1,4 +1,7 @@
 board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
+board_runner_args(jlink "--device=nRF52840" "--speed=4000")
+board_runner_args(uf2 "--family=0xADA52840")
 
 include(${ZEPHYR_BASE}/boards/common/uf2.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
